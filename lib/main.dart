b4be29ebe2088382f28330a411b45d2273c02106
dongjunkun <1440418246@qq.com>
2018-12-05 17:30:50 +0800

    simple slide drawer

--- lib/main.dart
+++ lib/main.dart
@@ -25,45 +25,103 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  Offset _horizontalDragStart;
+  AnimationController _sideAnimController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _sideAnimController =
+        AnimationController(duration: Duration(milliseconds: 500), vsync: this)
+          ..addListener(() {
+            setState(() {});
+          });
+  }
+
+  @override
+  void dispose() {
+    _sideAnimController.dispose();
+    super.dispose();
+  }
+
+  void _onHorizontalDragStart(DragStartDetails details) {
+    _horizontalDragStart = details.globalPosition;
+  }
+
+  void _onHorizontalDragUpdate(DragUpdateDetails details) {
+    if(_sideAnimController.isAnimating)
+      return;
+    _sideAnimController.value +=
+        details.primaryDelta/(MediaQuery.of(context).size.width??details.primaryDelta);
+//    details.primaryDelta/(MediaQuery.of(context).size.width??details.primaryDelta)
+//    print(_sideAnimController.value);
+  }
+
+  void _onHorizontalDragEnd(DragEndDetails details) {
+    _horizontalDragStart = null;
+    if(_sideAnimController.isCompleted){
+      return;
+    }
+    if(details.primaryVelocity > 0){
+      _sideAnimController.forward();
+    }else{
+      _sideAnimController.reverse();
+    }
+//    print(details.primaryVelocity );
+
+  }
+
+  Animation<double> _scaleRect() {
+    return Tween<double>(begin: 1.5, end: 1).animate(_sideAnimController);
+  }
+
+  Animation<RelativeRect> _offsetRect() {
+    double width = MediaQuery.of(context).size.width;
+//    return Tween<RelativeRect>(
+//            begin: RelativeRect.fromLTRB(0, 0, 0, 0),
+//            end: RelativeRect.fromLTRB(width - 80, 0, 80 - width, 0))
+//        .animate(_sideAnimController);
+  return RelativeRectTween( begin: RelativeRect.fromLTRB(0, 0, 0, 0),
+         end: RelativeRect.fromLTRB(width - 80, 0, 80 - width, 0)).animate(_sideAnimController);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
         body: GestureDetector(
-          onHorizontalDragStart: (DragStartDetails details) {
-            print(details.globalPosition);
-          },
-          onHorizontalDragCancel: () {
-
-          },
-          onHorizontalDragDown: (DragDownDetails details) {},
-          onHorizontalDragUpdate: (DragUpdateDetails details) {
-            print(details.globalPosition);
-          },
-          onHorizontalDragEnd: (DragEndDetails details) {
-            print(details.velocity);
-          },
-          child: Stack(
-            children: <Widget>[
-              Container(
-                color: Colors.red,
-                child: Center(
-                  child: Text("A"),
-                ),
-              ),
-              Container(
-                color: Colors.blue,
-                child: Center(
-                  child: Text("B"),
-                ),
-              )
-            ],
-          ),
-        ));
+            behavior: HitTestBehavior.opaque,
+            onHorizontalDragStart: _onHorizontalDragStart,
+            onHorizontalDragUpdate: _onHorizontalDragUpdate,
+            onHorizontalDragEnd: _onHorizontalDragEnd,
+            child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                return Stack(
+                  children: <Widget>[
+                    ScaleTransition(
+                      scale: _scaleRect(),
+                      child: Container(
+                        color: Colors.blue,
+                        child: Center(
+                          child: Text("B"),
+                        ),
+                      ),
+                    ),
+                    PositionedTransition(
+                      rect: _offsetRect(),
+                      child: Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
+                        color: Colors.red,
+                        child: Center(
+                          child: Text("A"),
+                        ),
+                      ),
+                    )
+                  ],
+                );
+              },
+            )));
   }
 }
