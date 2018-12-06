d74b441b53b491b7b4517dd1f8846486469005f5
dongjunkun <1440418246@qq.com>
2018-12-06 16:33:36 +0800

    侧滑3d效果

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math' show pi;
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -11,30 +13,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'flutter 3d sideslip menu'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  Offset _horizontalDragStart;
   AnimationController _sideAnimController;
 
   @override
   void initState() {
     super.initState();
-
     _sideAnimController =
-        AnimationController(duration: Duration(milliseconds: 500), vsync: this)
+        AnimationController(duration: Duration(milliseconds: 250), vsync: this)
           ..addListener(() {
             setState(() {});
           });
@@ -46,82 +44,84 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     super.dispose();
   }
 
-  void _onHorizontalDragStart(DragStartDetails details) {
-    _horizontalDragStart = details.globalPosition;
-  }
+  void _onHorizontalDragStart(DragStartDetails details) {}
 
   void _onHorizontalDragUpdate(DragUpdateDetails details) {
-    if(_sideAnimController.isAnimating)
-      return;
-    _sideAnimController.value +=
-        details.primaryDelta/(MediaQuery.of(context).size.width??details.primaryDelta);
-//    details.primaryDelta/(MediaQuery.of(context).size.width??details.primaryDelta)
-//    print(_sideAnimController.value);
+    if (_sideAnimController.isAnimating) return;
+    _sideAnimController.value += details.primaryDelta /
+        (MediaQuery.of(context).size.width ?? details.primaryDelta);
   }
 
   void _onHorizontalDragEnd(DragEndDetails details) {
-    _horizontalDragStart = null;
-    if(_sideAnimController.isCompleted){
+    if (_sideAnimController.isCompleted) {
       return;
     }
-    if(details.primaryVelocity > 0){
-      _sideAnimController.forward();
-    }else{
-      _sideAnimController.reverse();
-    }
-//    print(details.primaryVelocity );
 
+    if (details.primaryVelocity > 1) {
+      _sideAnimController.forward();
+    } else if (details.primaryVelocity < -1) {
+      _sideAnimController.reverse();
+    } else {
+      if (_sideAnimController.value >= 0.5) {
+        _sideAnimController.forward();
+      } else {
+        _sideAnimController.reverse();
+      }
+    }
   }
 
   Animation<double> _scaleRect() {
-    return Tween<double>(begin: 1.5, end: 1).animate(_sideAnimController);
+    return Tween<double>(begin: 1.8, end: 1).animate(_sideAnimController);
   }
 
-  Animation<RelativeRect> _offsetRect() {
+  /* Animation<RelativeRect> _offsetRect() {
     double width = MediaQuery.of(context).size.width;
-//    return Tween<RelativeRect>(
-//            begin: RelativeRect.fromLTRB(0, 0, 0, 0),
-//            end: RelativeRect.fromLTRB(width - 80, 0, 80 - width, 0))
-//        .animate(_sideAnimController);
-  return RelativeRectTween( begin: RelativeRect.fromLTRB(0, 0, 0, 0),
-         end: RelativeRect.fromLTRB(width - 80, 0, 80 - width, 0)).animate(_sideAnimController);
-  }
+    return RelativeRectTween(
+            begin: RelativeRect.fromLTRB(0, 0, 0, 0),
+            end: RelativeRect.fromLTRB(width - 80, 0, 80 - width, 0))
+        .animate(_sideAnimController);
+  }*/
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
         body: GestureDetector(
             behavior: HitTestBehavior.opaque,
             onHorizontalDragStart: _onHorizontalDragStart,
             onHorizontalDragUpdate: _onHorizontalDragUpdate,
             onHorizontalDragEnd: _onHorizontalDragEnd,
-            child: LayoutBuilder(
-              builder: (BuildContext context, BoxConstraints constraints) {
-                return Stack(
-                  children: <Widget>[
-                    ScaleTransition(
-                      scale: _scaleRect(),
-                      child: Container(
-                        color: Colors.blue,
-                        child: Center(
-                          child: Text("B"),
-                        ),
-                      ),
+            child: Stack(
+              children: <Widget>[
+                ScaleTransition(
+                  scale: _scaleRect(),
+                  child: Container(
+                    height: height,
+                    width: width,
+                    child: Image.asset(
+                      'images/stars.png',
+                      fit: BoxFit.cover,
                     ),
-                    PositionedTransition(
-                      rect: _offsetRect(),
-                      child: Container(
-                        height: MediaQuery.of(context).size.height,
-                        width: MediaQuery.of(context).size.width,
-                        color: Colors.red,
-                        child: Center(
-                          child: Text("A"),
-                        ),
-                      ),
-                    )
-                  ],
-                );
-              },
+                  ),
+                ),
+                Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.identity()
+                    ..setEntry(3, 2, 0.008)
+                    ..translate(_sideAnimController.value * (width - 80), 0, 0)
+                    ..rotateY(pi * _sideAnimController.value * 0.05)
+                    ..scale(1 - _sideAnimController.value * 0.15),
+                  child: Container(
+                    height: height,
+                    width: width,
+                    child: Image.asset(
+                      'images/balloon.png',
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ],
             )));
   }
 }
